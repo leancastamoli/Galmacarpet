8e92b66008ef9bb1fef04110c39afe2eedbaee33
Leandro Castaño <leancastamoli@gmail.com>
2020-02-27 10:52:22 +0100

    comienzo de html

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <title>Galmacarpet</title>
+</head>
+<body>
+    <header>
+		<div class="menu">
+            <ul>
+                <li>¿Quienes somos?</li>
+                <li>Instalaciones</li>
+                <li>Catalogo</li>
+                <li>Contacta con nosotros</li>
+            </ul>
+        </div>
+    </header>
+        <main>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
+            Vel modi aperiam ut eligendi deserunt voluptatum laudantium nisi <br>
+            dolor ab quod fuga cumque ipsa optio debitis ex, aspernatur vero<br>
+            nobis mollitia?<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+            Hic labore officia ex quis, dolores perspiciatis excepturi <br>
+            aspernatur vel porro numquam temporibus recusandae voluptatum <br>
+            inventore, et necessitatibus. Rem cumque autem assumenda!<br>
+        </main>
+</body>
+</html>
