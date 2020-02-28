ece2bcb78f45183285891246c7317b5860d43ef3
Leandro Castaño <leancastamoli@gmail.com>
2020-02-28 13:38:48 +0100

    Pagina principal hecha menos contacta con nosotros

--- index.php
+++ index.php
@@ -29,13 +29,39 @@
             <div class="somos">
                 <h2>¿Quienes somos?</h2>
                 <img src="assets/img/quienes_somos.png" alt="¿Quienes Somos?">
-                <br>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
                     Fuga quisquam aliquid earum eligendi natus cupiditate, <br>
                     voluptatibus quaerat totam porro inventore pariatur quos ullam <br>
                     beatae magni praesentium asperiores in sapiente nostrum.
+                    <button class="botont">¿Quienes somos?</button>
                 </p>
+                
+            </div>
+            <div class="instal">
+                <h2>Instalaciones</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+                    Fuga quisquam aliquid earum eligendi natus cupiditate, <br>
+                    voluptatibus quaerat totam porro inventore pariatur quos ullam <br>
+                    beatae magni praesentium asperiores in sapiente nostrum.
+                    <button class="botont">Instalaciones</button>
+                </p>
+                <img src="assets/img/instalacion.jpg" alt="Instalacion">
+                
+                
+            </div>
+            <div class="catalogo">
+                <h2>Catalogo?</h2>
+                <img src="assets/img/catalogo.jpg" alt="Catalogo">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+                    Fuga quisquam aliquid earum eligendi natus cupiditate, <br>
+                    voluptatibus quaerat totam porro inventore pariatur quos ullam <br>
+                    beatae magni praesentium asperiores in sapiente nostrum.
+                    <button class="botont">Ir al catalogo</button>
+                </p>
+                
             </div>
                 
         </main>
