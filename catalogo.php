79a49a3ecc3e4bc4dbe95be19180557ce6321796
Leandro Castaño <leancastamoli@gmail.com>
2020-03-11 14:25:24 +0100

    funcionamiento correcto del catalogo

--- catalogo.php
+++ catalogo.php
@@ -5,13 +5,18 @@
         $producto = new Producto();
         $producto->delete($id);
     }
+    if (isset($_GET['tipo'])) {
+        if ($_GET['tipo']=='pasillo') {
+            # code...
+        }
+    }
 ?>
 
 <main>
     <div class="Ccontenedor">
 
         <div class="Ctitulo">
-            <h1>Catalogo</h1>
+             <a href="index.php?p=catalogo"><h1>Catalogo</h1></a> 
         </div>
         <div class="Cdatos">
             <div class="filtro">
@@ -26,16 +31,45 @@
                 ?>
                 <div class="rutas">
                     <ul>
-                        <li>PASILLOS</li>
-                        <ul>
-                            <li>HAUSSMANN 1G</li>
-                            <li>HAUSSMANN 2G</li>
-                            <li>HAUSSMANN 3G</li>
-                            <li>HAUSSMANN 4G</li>
-                            <li>HAUSSMANN 5G</li>
-                        </ul>
-                        <li>CUSTOM RUG</li>
-                        <li>MOQUETA</li>
+                        <div class="f1">
+                            <li> <a href="index.php?p=catalogo&tipo=pasillos">PASILLOS</a> 
+                                <ul>
+                                    <li> <a href="index.php?p=catalogo&tipo=pasillos&subtipo=1g">HAUSSMANN 1G</a></li>
+                                    <li> <a href="index.php?p=catalogo&tipo=pasillos&subtipo=2g"> HAUSSMANN 2G</a></li>
+                                    <li> <a href="index.php?p=catalogo&tipo=pasillos&subtipo=3g"> HAUSSMANN 3G</a></li>
+                                    <li> <a href="index.php?p=catalogo&tipo=pasillos&subtipo=4g"> HAUSSMANN 4G</a></li>
+                                    <li> <a href="index.php?p=catalogo&tipo=pasillos&subtipo=5g"> HAUSSMANN 5G</a></li>
+                                </ul>
+                            </li>
+                        </div>
+                        <div class="f1">
+                            <li><a href="index.php?p=catalogo&tipo=custom_rug">CUSTOM RUG</a></li>
+                        </div>
+                        <div class="f2">
+                            <li><a href="index.php?p=catalogo&tipo=moqueta">MOQUETA</a>
+                                <ul>
+                                    <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract">CONTRACT</a>
+                                        <ul>
+                                            <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract&modelo=custom_carpet">CUSTOM CARPETS</a>
+                                                <ul>
+                                                    <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract&modelo=custom_carpet&variacion=1g">VARIATION 1G</a></li>
+                                                    <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract&modelo=custom_carpet&variacion=2g">VARIATION 2G</a></li>
+                                                    <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract&modelo=custom_carpet&variacion=3g">VARIATION 3G</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract&modelo=cobalt">COBALT</a></li>
+                                            <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=contract&modelo=zem">ZEM</a></li>
+                                        </ul>
+                                    </li>
+                                    <li> <a href="index.php?p=catalogo&tipo=moqueta&subtipo=residencial">RESIDENCIAL</a>
+                                        <ul>
+                                            <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=residencial&modelo=renaissance">RENAISSANCE</a></li>
+                                            <li><a href="index.php?p=catalogo&tipo=moqueta&subtipo=residencial&modelo=infinity">INFINITY</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </div>
                     </ul>
                 </div>
                 
@@ -46,24 +80,189 @@
                     $producto->get();
                     $productos = $producto->get_rows();
                     foreach ($productos as $fila) {
-                    ?>
-                        <div class="imagen">
-                            <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
-                            <div class="controles">
-                    <?php
-                            if (isset($_SESSION['tipo'])) {
-                                if (isset($_SESSION['tipo'])) {
-                    ?>
-                                <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
-                                <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
-                    <?php
+                        if (isset($_GET['tipo'])) {
+                            $tipo = $_GET['tipo'];
+                            if ($tipo == 'pasillos') {
+                                if ($fila['tipo'] == $tipo) {
+                                    if (isset($_GET['subtipo'])) {
+                                        if ($fila['subtipo'] == $_GET['subtipo']) {
+                                            ?>
+                                                <div class="imagen">
+                                                    <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                <div class="controles">
+                                            <?php
+                                                    if (isset($_SESSION['tipo'])) {
+                                                        if (isset($_SESSION['tipo'])) {
+                                            ?>
+                                                        <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                        <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                            <?php
+                                                        }
+                                                    }
+                                            ?>
+                                                    </div>
+                                                    
+                                                </div>
+                                            <?php 
+                                        }
+                                    }else{
+                                            ?>
+                                                <div class="imagen">
+                                                    <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                <div class="controles">
+                                            <?php
+                                                    if (isset($_SESSION['tipo'])) {
+                                                        if (isset($_SESSION['tipo'])) {
+                                            ?>
+                                                        <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                        <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                            <?php
+                                                        }
+                                                    }
+                                            ?>
+                                                    </div>
+                                                    
+                                                </div>
+                                            <?php 
+                                    }
+                                }else{
+                                    /* No hay productos pasilleros */
+                                }
+                                
+                            }else if($tipo == 'custom_rug'){
+                                if ($fila['tipo'] == $tipo) {
+                                            ?>
+                                                <div class="imagen">
+                                                    <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                <div class="controles">
+                                            <?php
+                                                    if (isset($_SESSION['tipo'])) {
+                                                        if (isset($_SESSION['tipo'])) {
+                                            ?>
+                                                        <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                        <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                            <?php
+                                                        }
+                                                    }
+                                            ?>
+                                                    </div>
+                                                    
+                                                </div>
+                                            <?php 
+                                }else{
+                                    /* No hay productos pasilleros */
+                                }
+                            }else if($tipo == 'moqueta'){
+                                if ($fila['tipo'] == $tipo) {
+                                    if (isset($_GET['subtipo'])) {
+                                        if ($fila['subtipo'] == $_GET['subtipo']) {
+                                            if (isset($_GET['modelo'])) {
+                                                if ($fila['subtipo'] == $_GET['subtipo'] && $fila['modelo'] == $_GET['modelo']) {
+                                                    if (isset($_GET['variacion'])) {
+                                                        if ($fila['subtipo'] == $_GET['subtipo'] && $fila['modelo'] == $_GET['modelo'] && $fila['variacion'] == $_GET['variacion']) {
+                                                            ?>
+                                                                <div class="imagen">
+                                                                    <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                                <div class="controles">
+                                                            <?php
+                                                                    if (isset($_SESSION['tipo'])) {
+                                                                        if (isset($_SESSION['tipo'])) {
+                                                            ?>
+                                                                        <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                                        <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                                            <?php
+                                                                        }
+                                                                    }
+                                                            ?>
+                                                                    </div>
+                                                                    
+                                                                </div>
+                                                            <?php
+                                                        }
+                                                    }else{
+                                                        ?>
+                                                            <div class="imagen">
+                                                                <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                            <div class="controles">
+                                                        <?php
+                                                                if (isset($_SESSION['tipo'])) {
+                                                                    if (isset($_SESSION['tipo'])) {
+                                                        ?>
+                                                                    <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                                    <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                                        <?php
+                                                                    }
+                                                                }
+                                                        ?>
+                                                                </div>
+                                                                
+                                                            </div>
+                                                        <?php  
+                                                    }
+                                                }
+                                            }else{
+                                                ?>
+                                                    <div class="imagen">
+                                                        <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                    <div class="controles">
+                                                <?php
+                                                        if (isset($_SESSION['tipo'])) {
+                                                            if (isset($_SESSION['tipo'])) {
+                                                ?>
+                                                            <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                            <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                                <?php
+                                                            }
+                                                        }
+                                                ?>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                <?php 
+                                            }
+                                        }
+                                    }else{
+                                            ?>
+                                                <div class="imagen">
+                                                    <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                                <div class="controles">
+                                            <?php
+                                                    if (isset($_SESSION['tipo'])) {
+                                                        if (isset($_SESSION['tipo'])) {
+                                            ?>
+                                                        <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                                        <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                                            <?php
+                                                        }
+                                                    }
+                                            ?>
+                                                    </div>
+                                                    
+                                                </div>
+                                            <?php 
+                                    }
                                 }
                             }
-                    ?>
-                            </div>
-                            
-                        </div>
-                    <?php
+                        }else{
+                            ?>
+                                <div class="imagen">
+                                    <a href="index.php?p=datos&id=<?php echo $fila['id']?>"><img src="./assets/img/<?php echo $fila['imagen']?>" alt="imagen"></a><br>
+                                    <div class="controles">
+                            <?php
+                                    if (isset($_SESSION['tipo'])) {
+                                        if (isset($_SESSION['tipo'])) {
+                            ?>
+                                        <a href="index.php?p=catalogo&borrar=<?php echo $fila['id']?>"><i class="far fa-trash-alt"></i></a>
+                                        <a href="index.php?p=datos&edit=<?php echo $fila['id']?>"><i class="fas fa-edit"></i></a>
+                            <?php
+                                        }
+                                    }
+                            ?>
+                                    </div>
+                                    
+                                </div>
+                            <?php
+                        }
                     }
                 ?>
             
