895568bf000c97fb9b2aed7904edf3605a051bb4
leandro <leancastamoli@gmail.com>
2020-04-23 16:20:43 +0200

    cambios estilos definitivos

--- instal.php
+++ instal.php
@@ -2,50 +2,47 @@
     <div class="ScontenedorR">
         <div class="Simagen">
             <img src="./assets/img/instal1.PNG" alt="instal1">
-            <div class="StituloR">
-                <div class="Sh1">
-                    <h1>EQUIPO</h1>
-                </div>
-                <div class="SparrafoR">
-                    <p>
-                    Contamos con un nutrido y profesional equipo humano a su servicio en las diferentes áreas de la firma cuya especial preocupación es atender, de forma óptima, cada necesidad específica del cliente.
-                    </p>
-                </div>
+        </div>
+        <div class="StituloR">
+            <div class="Sh1">
+                <h1>EQUIPO</h1>
+            </div>
+            <div class="SparrafoR">
+                <p>
+                Contamos con un nutrido y profesional equipo humano a su servicio en las diferentes áreas de la firma cuya especial preocupación es atender, de forma óptima, cada necesidad específica del cliente.
+                </p>
             </div>
         </div>
-        
     </div>
     
     <div class="ScontenedorR">
         <div class="Simagen">
             <img src="./assets/img/instal2.PNG" alt="instal1">
-            <div class="StituloR2">
-                <div class="Sh1">
-                    <h1>ÁREA DE DISEÑO</h1>
-                </div>
-                <div class="SparrafoR">
-                    <p>
-                    La empresa dispone de un departamento propio de diseño como eslabón fundamental del área de I+D+i. Cada año incorpora a la oferta de la compañía nuevas colecciones atendiendo a la información suministrada por nuestros clientes. Igualmente, se cubren proyectos individuales a medida. Para ello, se utiliza tecnología informática de última generación que permite un estricto control de las instalaciones productivas y las materias primas en beneficio de unos excelentes resultados de creatividad y acabados finales.
-                    </p>
-                </div>
-            </div>
         </div>
-        
+        <div class="StituloR">
+            <div class="Sh1">
+                <h1>ÁREA DE DISEÑO</h1>
+            </div>
+            <div class="SparrafoR">
+                <p>
+                La empresa dispone de un departamento propio de diseño como eslabón fundamental del área de I+D+i. Cada año incorpora a la oferta de la compañía nuevas colecciones atendiendo a la información suministrada por nuestros clientes. Igualmente, se cubren proyectos individuales a medida. Para ello, se utiliza tecnología informática de última generación que permite un estricto control de las instalaciones productivas y las materias primas en beneficio de unos excelentes resultados de creatividad y acabados finales.
+                </p>
+            </div>
+        </div>   
     </div>
     <div class="ScontenedorR">
         <div class="Simagen">
             <img src="./assets/img/instal3.PNG" alt="instal1">
-            <div class="StituloR3">
-                <div class="Sh1">
-                    <h1>TELARES</h1>
-                </div>
-                <div class="SparrafoR">
-                    <p>
-                    Nuestro sistema productivo incorpora las últimas novedades tecnológicas en cuanto a diseño, el Sistema Jacquard Electrónico, lo que les confiere una alta flexibilidad y una enorme capacidad de adaptación para cubrir las necesidades del cliente.
-                    </p>
-                </div>
-            </div>
         </div>
-        
+        <div class="StituloR">
+            <div class="Sh1">
+                <h1>TELARES</h1>
+            </div>
+            <div class="SparrafoR">
+                <p>
+                Nuestro sistema productivo incorpora las últimas novedades tecnológicas en cuanto a diseño, el Sistema Jacquard Electrónico, lo que les confiere una alta flexibilidad y una enorme capacidad de adaptación para cubrir las necesidades del cliente.
+                </p>
+            </div>
+        </div>  
     </div>
 </main>
--- somos.php
+++ somos.php
@@ -1,18 +1,17 @@
 <main>
-    <div class="Scontenedor">
-        <div class="Simagen">
+    <div class="ScontenedorS">
+        <div class="SimagenS">
             <img src="./assets/img/somos1.PNG" alt="somos1">
-            <div class="Stitulo">
-                <div class="Sh1">
-                    <h1>GALMACARPET</h1>
-                </div>
-                <div class="Sparrafo">
-                    <p>
-                    SUALSA es, sin duda, un referente en la fabricación de alfombras y moquetas en España. Una empresa que ha sabido mantener y aumentar su prestigio en el sector gracias al Know-how adquirido por sus recursos humanos y a la iniciativa empresarial, que ha permitido contar con un equipo propio de I+D+i permitiéndole reforzar su liderazgo año tras año en las distintas tendencias y áreas del mercado (tradicional, vanguardista, doméstico, profesional y contract).
-                    </p>
-                </div>
+        </div>
+        <div class="StituloS">
+            <div class="Sh1S">
+                <h1>GALMACARPET</h1>
+            </div>
+            <div class="SparrafoS">
+                <p>
+                SUALSA es, sin duda, un referente en la fabricación de alfombras y moquetas en España. Una empresa que ha sabido mantener y aumentar su prestigio en el sector gracias al Know-how adquirido por sus recursos humanos y a la iniciativa empresarial, que ha permitido contar con un equipo propio de I+D+i permitiéndole reforzar su liderazgo año tras año en las distintas tendencias y áreas del mercado (tradicional, vanguardista, doméstico, profesional y contract).
+                </p>
             </div>
         </div>
-        
     </div>
 </main>
